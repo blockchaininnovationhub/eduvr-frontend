d3be614eb9b8461889c1bf2bde279140ae0595e7
olagunjuea1 <olagunjuea1@gmail.com>
2024-08-26 21:42:45 +0100

    🚀  Modify Dashboard

--- src/components/dashboard/DashboardLayout.jsx
+++ src/components/dashboard/DashboardLayout.jsx
@@ -90,7 +90,7 @@ const DashbardLayout = () => {
                         </Badge>
                     </Link>
                     <Link
-                        href="#"
+                        href="./Coming-soon"
                         className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
                         <LineChart className="h-4 w-4" />
                         Assignments/Polls
