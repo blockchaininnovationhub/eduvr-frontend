9a3e24723309b099a7b43567dc6159724d6f42fa
olagunjuea1 <olagunjuea1@gmail.com>
2024-08-27 18:07:53 +0100

    🎉  Update chnages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,34 +8,28 @@ const nunito = Nunito_Sans({
   weight: ['200', '300', '400', '500', '600', '700'],
 })
 
-// import '@rainbow-me/rainbowkit/styles.css';
+import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { WagmiProvider } from 'wagmi';
-// import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
 import { config } from '../wagmi';
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // return (
-  //   <WagmiProvider config={config}>
-  //     <QueryClientProvider client={queryClient}>
-  //       <RainbowKitProvider modalSize="compact">
-  //         <main className={`${nunito.className}`}>
-  //           <Component {...pageProps} />
-  //         </main>
-  //       </RainbowKitProvider>
-  //     </QueryClientProvider>
-  //   </WagmiProvider>
-  // );
-
   return (
-    <main className={`${nunito.className}`}>
-      <Component {...pageProps} />
-    </main>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider modalSize="compact">
+          <main className={`${nunito.className}`}>
+            <Component {...pageProps} />
+          </main>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
