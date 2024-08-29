f3e4edf15f635904196c6ce4d2d8a973c926e8f2
iamoracle <38940247+iamoracle@users.noreply.github.com>
2024-08-29 18:24:18 +0100

    Hello World

--- src/components/class/StudentBoard.jsx
+++ src/components/class/StudentBoard.jsx
@@ -2,7 +2,6 @@ import { useEffect, useRef } from "react";
 import Peer from "peerjs";
 
 export const StudentBoard = () => {
-  const localRef = useRef(null);
   const remoteRef = useRef(null);
   const currentPeer = useRef(null);
 
@@ -12,15 +11,7 @@ export const StudentBoard = () => {
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: true })
       .then((stream) => {
-        localRef.current.srcObject = stream;
-
-        localRef.current.onloadedmetadata = () => {
-          localRef.current.play();
-        };
-
         const call = currentPeer.current.call(remotePeerId, stream);
-
-        console.log(call);
 
         call.on("stream", (remoteStream) => {
           console.log("Received stream");
@@ -63,11 +54,6 @@ export const StudentBoard = () => {
 
   return (
     <div className="w-[1700px] h-[415px] flex flex-row relative overflow-y-auto bg-slate-900">
-      <video
-        ref={localRef}
-        className="w-1/3 h-full object-cover"
-        controls
-      ></video>
       <video
         ref={remoteRef}
         src="/Venice_5.mp4"
