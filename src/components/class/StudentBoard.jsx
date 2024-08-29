8fcc834a9590907202813a0603aac86b306c9c42
iamoracle <38940247+iamoracle@users.noreply.github.com>
2024-08-29 18:37:22 +0100

    🐛 added media stream

--- src/components/class/StudentBoard.jsx
+++ src/components/class/StudentBoard.jsx
@@ -5,31 +5,61 @@ export const StudentBoard = () => {
   const remoteRef = useRef(null);
   const currentPeer = useRef(null);
 
+  const createMediaStreamFake = () => {
+    return new MediaStream([
+      createEmptyAudioTrack(),
+      createEmptyVideoTrack({ width: 640, height: 480 }),
+    ]);
+  };
+
+  const createEmptyAudioTrack = () => {
+    const ctx = new AudioContext();
+    const oscillator = ctx.createOscillator();
+    const dst = oscillator.connect(ctx.createMediaStreamDestination());
+    oscillator.start();
+    const track = dst.stream.getAudioTracks()[0];
+    return Object.assign(track, { enabled: false });
+  };
+
+  const createEmptyVideoTrack = ({ width, height }) => {
+    const canvas = Object.assign(document.createElement("canvas"), {
+      width,
+      height,
+    });
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, width, height);
+
+    const stream = canvas.captureStream();
+    const track = stream.getVideoTracks()[0];
+
+    return Object.assign(track, { enabled: false });
+  };
+
   const call = (remotePeerId) => {
     console.log("Calling " + remotePeerId);
 
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
-      .then((stream) => {
-        const call = currentPeer.current.call(remotePeerId, stream);
+    const call = currentPeer.current.call(
+      remotePeerId,
+      createMediaStreamFake()
+    );
 
-        call.on("stream", (remoteStream) => {
-          console.log("Received stream");
-          remoteRef.current.srcObject = remoteStream;
+    call.on("stream", (remoteStream) => {
+      console.log("Received stream");
+      remoteRef.current.srcObject = remoteStream;
 
-          remoteRef.current.onloadedmetadata = () => {
-            remoteRef.current.play();
-          };
-        });
+      remoteRef.current.onloadedmetadata = () => {
+        remoteRef.current.play();
+      };
+    });
 
-        call.on("close", () => {
-          console.log("Call ended");
-        });
+    call.on("close", () => {
+      console.log("Call ended");
+    });
 
-        call.on("error", (err) => {
-          console.error("Call error:", err);
-        });
-      });
+    call.on("error", (err) => {
+      console.error("Call error:", err);
+    });
   };
 
   useEffect(() => {
