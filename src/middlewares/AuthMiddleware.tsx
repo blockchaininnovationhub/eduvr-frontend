08c05d7e0f56a30964a49faa787f9d5b95229778
iamoracle <38940247+iamoracle@users.noreply.github.com>
2024-08-30 08:00:11 +0100

    fixed preloader

--- src/middlewares/AuthMiddleware.tsx
+++ src/middlewares/AuthMiddleware.tsx
@@ -1,12 +1,14 @@
 import { ReactNode, useEffect } from "react";
 import { useRouter } from "next/router";
 import { getProfile } from "@/utils/call";
+import useLoading from "@/hooks/useLoading";
 
 interface AuthMiddlewareProps {
   children: ReactNode;
 }
 const AuthMiddleware = ({ children }: AuthMiddlewareProps) => {
   const router = useRouter();
+  const { stopLoading, isLoading } = useLoading(true);
 
   useEffect(() => {
     const checkProfile = async () => {
@@ -15,12 +17,13 @@ const AuthMiddleware = ({ children }: AuthMiddlewareProps) => {
       if (profile === null) {
         router.push("/login");
       }
+      stopLoading();
     };
 
     checkProfile();
   }, [router]);
 
-  return children;
+  return isLoading ? <p>Preloader...</p> : <>{children}</>;
 };
 
 export default AuthMiddleware;
