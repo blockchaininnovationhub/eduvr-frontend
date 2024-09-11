bc03fecb0b5cfd6f7c7eb64edc002a8c21577bd5
iamoracle <38940247+iamoracle@users.noreply.github.com>
2024-09-11 20:22:13 +0100

    🐛 added edu chain

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -4,6 +4,6 @@ import { BaseTestnet, EDUChainTestnet, LiskTestnet } from "./chain";
 export const config = getDefaultConfig({
   appName: "EduVR",
   projectId: "d4a6f4edae5c0b3b2352b1e169c92123",
-  chains: [BaseTestnet, LiskTestnet, EDUChainTestnet],
+  chains: [EDUChainTestnet],
   ssr: true,
 });
